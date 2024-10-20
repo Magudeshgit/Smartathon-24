740133b094ca07dd9e793608d2086f1ec7a8e3a0
Magudeshgit <magudesh2006@gmail.com>
2024-10-21 00:38:45 +0530

    Minor UI Corrections

--- smartreg/settings.py
+++ smartreg/settings.py
@@ -25,7 +25,7 @@ print(__file__)
 SECRET_KEY = 'django-insecure-l*wml*eo)qn9s3@f++)b*!q1$1)6c4sd@bwksvu%3e@xgh4cfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://3.109.10.58', 'https://src.drmcet.ac.in']
