44e5eff41d56c9f464680f5515de108804ab649c
Magudeshgit <magudesh2006@gmail.com>
2024-10-21 19:07:43 +0530

    Export Functionality

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import statements, events, registrations, hackathonreg
+from import_export.admin import ExportActionMixin
 
-# Register your models here.
-admin.site.register([statements, events, registrations, hackathonreg])
+class RegistrationExport(ExportActionMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register([statements, events, hackathonreg])
+admin.site.register(registrations, RegistrationExport)
 admin.site.site_header = "Smartathon'24"
 admin.site.site_title = "Smartathon'24"
 
--- smartreg/settings.py
+++ smartreg/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'core',
     
     'tailwind',
-    'theme'
+    'theme',
+    
+    'import_export'
 ]
 
 MIDDLEWARE = [
